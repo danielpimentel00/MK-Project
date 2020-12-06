60b8cef1aabfe60b3794e5b79f17c6af1846a241
Daniel Pimentel <danielpimentel19@gmail.com>
2020-12-06 15:59:47 -0400

    control velocidad sprite

--- MortalKombat/Game1.cs
+++ MortalKombat/Game1.cs
@@ -6,12 +6,14 @@ namespace MortalKombat
 {
     public class Game1 : Game
     {
-        Texture2D backgroundTexture;
-
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-        private int anchuraConsola;
-        private int alturaConsola;
+        private Texture2D background;
+        public int anchuraConsola = 1280, alturaConsola = 720;
+
+
+        private Texture2D jax;
+        private AnimatedSprite fightingStance_jax;
 
         public Game1()
         {
@@ -24,9 +26,10 @@ namespace MortalKombat
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            graphics.PreferredBackBufferWidth = 1920;
-            graphics.PreferredBackBufferHeight = 1080; 
-            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = anchuraConsola;
+            graphics.PreferredBackBufferHeight = alturaConsola;
+            
+            graphics.IsFullScreen = false;
             graphics.ApplyChanges();
             // TODO: use this.Content to load your game content here
             base.Initialize();
@@ -37,7 +40,9 @@ namespace MortalKombat
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            backgroundTexture = Content.Load<Texture2D>("background02");
+            background = Content.Load<Texture2D>(@"Sprites/background02");
+            jax = Content.Load<Texture2D>(@"Sprites/stance_jax");
+            fightingStance_jax = new AnimatedSprite(jax, 2, 4);
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,19 +50,27 @@ namespace MortalKombat
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            if (Keyboard.GetState().IsKeyDown(Keys.F1))
+                graphics.ToggleFullScreen();
+
             // TODO: Add your update logic here
+
+            fightingStance_jax.Update();
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, 1920, 1080), Color.White);
+            spriteBatch.Draw(background, new Rectangle(0, 0, anchuraConsola, alturaConsola), Color.White);
+
             spriteBatch.End();
+
+            fightingStance_jax.Draw(spriteBatch, new Vector2(200, 520));
             base.Draw(gameTime);
         }
     }
